48eb076ced0a6e43bc5409b734db9dd41af7ea62
alexanderthegreat96 <alexandrulupaescu@gmail.com>
2026-01-15 11:53:24 +0200

    updated some code

--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,51 @@ mod core;
 use crate::core::afk_executor;
 use std::io::{self, Write};
 
-// intended to add \r\n after every log entry line on screen
-struct RawWriter<W: Write>(W);
+// this is the type of stuff that gives me headaches in rust
+// had to actually scratch my brain to do this
+// what this RawWriter + implementation does is enable
+// me to properly terminate each output to the terminal
+// as well as add a prefix to the logging
+// this is because enabling_raw_mode can cause
+// some serious formatting issues and screen the terminal session
+struct RawWriter<W: Write> {
+    inner: W,
+    new_line: bool,
+}
+
+impl<W: Write> RawWriter<W> {
+    fn new(inner: W) -> Self {
+        Self {
+            inner,
+            new_line: true,
+        }
+    }
+}
 
 impl<W: Write> Write for RawWriter<W> {
     fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        // proper line terminations so we don't fuck the terminal aspect
         for &byte in buf {
-            if byte == b'\n' {
-                self.0.write_all(b"\r")?;
+            // write a prefix
+            if self.new_line {
+                self.inner.write_all(b"[no-sleep] ")?;
+                self.new_line = false;
             }
-            self.0.write_all(&[byte])?;
+
+            // write term char
+            if byte == b'\n' {
+                self.inner.write_all(b"\r")?;
+                self.new_line = true;
+            }
+
+            self.inner.write_all(&[byte])?;
         }
         Ok(buf.len())
     }
 
     fn flush(&mut self) -> io::Result<()> {
-        self.0.flush()
+        self.inner.flush()
     }
 }
-
 fn main() {
     tracing_subscriber::fmt()
         .with_level(true)
@@ -29,7 +54,7 @@ fn main() {
         .with_thread_names(true)
         .without_time()
         .with_ansi(true)
-        .with_writer(|| RawWriter(io::stderr()))
+        .with_writer(|| RawWriter::new(io::stderr()))
         .init();
 
     tracing::info!("Starting No-Sleep-RS v0.0.1");
